91a8ede79fb38fa05984b4557a7e14a63cb0e768
Alessia Masola <alessiamasola@glaroam2-137-7.wireless.gla.ac.uk>
2018-11-24 12:57:47 +0000

    We can now start adding templates

--- gettogether/gettogether/settings.py
+++ gettogether/gettogether/settings.py
@@ -1,3 +1,10 @@
+import os
+
+SETTINGS_DIR = os.path.dirname(__file__)
+PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+
 # Django settings for gettogether project.
 
 DEBUG = True
@@ -108,9 +115,11 @@ ROOT_URLCONF = 'gettogether.urls'
 WSGI_APPLICATION = 'gettogether.wsgi.application'
 
 TEMPLATE_DIRS = (
+
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
 )
 
 INSTALLED_APPS = (
