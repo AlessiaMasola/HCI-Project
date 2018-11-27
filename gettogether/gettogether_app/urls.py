b04faf5d323f66f64e1822e76cd89e5fc6d00f30
Alessia Masola <alessiamasola@Alessias-MacBook-Pro.local>
2018-11-27 19:14:42 +0000

    Added useful links page and started working on forum page. Fixed minor issues with events page.

--- gettogether/gettogether_app/urls.py
+++ gettogether/gettogether_app/urls.py
@@ -4,4 +4,6 @@ from gettogether_app import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^events/$', views.events, name='events'),
-        url(r'^games/$', views.games, name='games'))
+        url(r'^games/$', views.games, name='games'),
+        url(r'^useful_links/$', views.useful_links, name='useful_links'),
+        url(r'^forum/$', views.forum, name='forum'))
--- gettogether/gettogether_app/views.py
+++ gettogether/gettogether_app/views.py
@@ -5,17 +5,21 @@ from django.shortcuts import render_to_response
 def index(request):
     # Request the context of the request.
     # The context contains information such as the client's machine details, for example.
-    context = RequestContext(request)
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render_to_response('gettogether_app/index.html', context_dict, context)
+    return render_to_response('gettogether_app/index.html')
 
 def events(request):
     return render_to_response('gettogether_app/events.html')
 
 def games(request):
     return render_to_response('gettogether_app/games.html')
+
+def useful_links(request):
+    return render_to_response('gettogether_app/useful_links.html')
+
+def forum(request):
+    return render_to_response('gettogether_app/forum.html')
